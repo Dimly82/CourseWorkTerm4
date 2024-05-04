60069ae6089b3c4920c4e99555d3eabf9ba816ce
Dimly82 <pdimochka@gmail.com>
2024-05-04 11:48:26 +0300

    create window for sort example

--- CourseWorkTerm4/CourseWorkTerm4.cpp
+++ CourseWorkTerm4/CourseWorkTerm4.cpp
@@ -5,14 +5,32 @@
 #include <string>
 
 constexpr auto MAX_LOAD_STRING = 100;
-constexpr auto ID_BUTTON_REGULAR = 1;
-constexpr auto ID_BUTTON_LAMBDA = 2;
+constexpr auto ID_BUTTON_SORT_EX = 1;
+constexpr auto ID_BUTTON_FEACH_EX = 2;
+constexpr auto ID_BUTTON_TRANSF_EX = 3;
+constexpr auto ID_RADBUTTON_SORT_ASC = 4;
+constexpr auto ID_RADBUTTON_SORT_DESC = 5;
+
+constexpr auto SORT_CLASS = L"SortWnd";
+constexpr auto FEACH_CLASS = L"FeachWnd";
+constexpr auto TRANSF_CLASS = L"TransfWnd";
 
 // Global Variables:
 HINSTANCE hInst; // current instance
 WCHAR szTitle[MAX_LOAD_STRING]; // The title bar text
 WCHAR szWindowClass[MAX_LOAD_STRING]; // the main window class name
-HWND textBox;
+
+// Window handlers
+HWND sortWnd;
+HWND feachWnd;
+HWND transfWnd;
+
+// Sort example components
+HWND sortExBtn;
+HWND sortExStat;
+HWND sortExInp;
+HWND sortExRBtn1;
+HWND sortExRBtn2;
 
 // Forward declarations of functions included in this code module:
 ATOM MyRegisterClass(HINSTANCE hInstance);
@@ -20,45 +38,9 @@ BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
-// Example functions
-int Square(int x) {
-	int res = 0;
-	for (size_t i = 0; i < 1000000000; i++) res += x;
-	return res;
-}
-
-auto lambdaSquare = [](int x) {
-	int res = 0;
-	for (size_t i = 0; i < 1000000000; i++) res += x;
-	return res;
-};
-
-// Callback functions
-void OnRegularFunction(HWND hWnd, int x) {
-	auto start = std::chrono::high_resolution_clock::now();
-	x = Square(x);
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-	std::string res = "Result: " + std::to_string(x) + "\nTime: " + std::to_string(duration.count()) + " microseconds";
-	std::wstring tmp = std::wstring(res.begin(), res.end());
-	LPCWSTR wres = tmp.c_str();
-
-	MessageBox(hWnd, wres, L"Regular Function Result", MB_OK);
-}
-
-void OnLambdaFunction(HWND hWnd, int x) {
-	auto start = std::chrono::high_resolution_clock::now();
-	x = lambdaSquare(x);
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-
-	std::string res = "Result: " + std::to_string(x) + "\nTime: " + std::to_string(duration.count()) + " microseconds";
-	std::wstring tmp = std::wstring(res.begin(), res.end());
-	LPCWSTR wres = tmp.c_str();
-
-	MessageBox(hWnd, wres, L"Regular Function Result", MB_OK);
-}
+// Sort window
+ATOM SortRegisterClass(HINSTANCE hInstance);
+LRESULT CALLBACK SortWndProc(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                       _In_opt_ HINSTANCE hPrevInstance,
@@ -71,6 +53,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOAD_STRING);
 	LoadStringW(hInstance, IDC_COURSEWORKTERM4, szWindowClass, MAX_LOAD_STRING);
 	MyRegisterClass(hInstance);
+	SortRegisterClass(hInstance);
 
 	// Perform application initialization:
 	if (!InitInstance(hInstance, nCmdShow)) {
@@ -146,29 +129,27 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	switch (message) {
 	case WM_CREATE: {
-		CreateWindow(L"BUTTON", L"Regular Function",
+		CreateWindow(L"BUTTON", L"Пример sort",
 		             WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10, 10, 200, 30, hWnd,
-		             (HMENU)ID_BUTTON_REGULAR, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
-		CreateWindow(L"BUTTON", L"Lambda Function",
+		             (HMENU)ID_BUTTON_SORT_EX, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+		CreateWindow(L"BUTTON", L"Пример for_each",
 		             WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10, 50, 200, 30, hWnd,
-		             (HMENU)ID_BUTTON_LAMBDA, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
-		textBox = CreateWindow(L"Edit", L"5", WS_CHILD | WS_VISIBLE | WS_BORDER, 230, 25, 200, 30, hWnd, NULL, NULL,
-		                       NULL);
+		             (HMENU)ID_BUTTON_FEACH_EX, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
+		CreateWindow(L"BUTTON", L"Пример transform",
+		             WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, 10, 90, 200, 30, hWnd,
+		             (HMENU)ID_BUTTON_TRANSF_EX, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), NULL);
 	}
 	break;
 	case WM_COMMAND: {
-		LPWSTR x = (LPWSTR)malloc(sizeof(wchar_t) * 100);
-		GetWindowText(textBox, x, 100);
-
 		int wmId = LOWORD(wParam);
 		// Parse the menu selections:
 		switch (wmId) {
-		case ID_BUTTON_REGULAR:
-			OnRegularFunction(hWnd, std::stoi(x));
-			break;
-		case ID_BUTTON_LAMBDA:
-			OnLambdaFunction(hWnd, std::stoi(x));
-			break;
+		case ID_BUTTON_SORT_EX: {
+			sortWnd = CreateWindow(SORT_CLASS, L"Пример sort", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 500, 250, hWnd,
+			                       nullptr, hInst, nullptr);
+			ShowWindow(sortWnd, SW_SHOW);
+		}
+		break;
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
@@ -178,20 +159,75 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
-
-		free(x);
 	}
 	break;
 	case WM_PAINT: {
 		PAINTSTRUCT ps;
 		HDC hdc = BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code that uses hdc here...
 		EndPaint(hWnd, &ps);
 	}
 	break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+ATOM SortRegisterClass(HINSTANCE hInstance) {
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = SortWndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_COURSEWORKTERM4));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = SORT_CLASS;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+LRESULT CALLBACK SortWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+	case WM_CREATE: {
+		sortExStat = CreateWindow(L"Static", L"Введите элементы-числа массива (через запятую, без пробелов)",
+		                          WS_CHILD | WS_VISIBLE, 10, 10, 460, 25, hWnd, nullptr,
+		                          (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), nullptr);
+		sortExInp = CreateWindow(L"Edit", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL, 10, 40, 460, 25,
+		                         hWnd, nullptr, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), nullptr);
+		sortExBtn = CreateWindow(L"Button", L"Запустить", WS_VISIBLE | WS_CHILD | WS_BORDER, 150, 170, 200, 30, hWnd,
+		                         nullptr, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), nullptr);
+		sortExRBtn1 = CreateWindow(L"Button", L"По возрастанию", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 10, 70, 240,
+		                           95, hWnd, nullptr, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), nullptr);
+		SendMessage(sortExRBtn1, BM_SETCHECK, BST_CHECKED, 0);
+		sortExRBtn2 = CreateWindow(L"Button", L"По убыванию", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 250, 70, 460,
+		                           95, hWnd, nullptr, (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE), nullptr);
+	}
+	break;
+	case WM_COMMAND: {
+		int wmId = LOWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId) {
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT: {
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+	}
+	break;
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
